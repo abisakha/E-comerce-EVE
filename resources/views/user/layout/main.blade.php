39a586c0eb4cdd87001003902d5f62f44acf862b
abisakha <abisakhasaif123@gmail.com>
2025-06-11 19:34:06 +0700

    Membuat Halaman Home Page

---
+++ resources/views/user/layout/main.blade.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eve</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body class="gambar">
+<!-- navbar -->
+    <nav class="navbar navbar-expand-md navbar-light">
+    <div class="container-fluid">
+        <img src="img/icon.png" width="120px">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="sale.html">Sale</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Purchases</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+            </ul>
+            <form class="navbar-nav">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+        <!-- konten iklan utama -->
+        <div class="container-sm mt-4 ">
+            <div class="clearfix">
+            <img src="img/my3.png" class="col-md-6 float-md-end mb-3 ms-md-3">
+                <p class="fontIklan">Discover Your</p>
+                    <p class="fontIklan">Style Today!</p>
+                    <p style="font-size: 19px;">Temukan gaya terbaikmu dengan koleksi busana yang elegan dan modern, hanya untukmu.</p>
+                <div class="row">
+                    <div class="col-1">
+                        <button type="button" class="btn btn-outline-secondary btn-lg" style="border-radius: 10px; width : 150px; height : 50px; ">Buy Now</button>
+                    </div>
+               </div>
+            </div>
+        </div>
+
+        <!-- Hot Deals For You -->
+           <div class="container-fluid mt-3">
+             <div class="row ">
+                 <div class="col-4 d-flex flex-column align-items-center justify-content-center text-center" >
+                      <img src="img/1.png" width="340px">
+                  </div>
+
+                  <div class="col-4 d-flex flex-column align-items-center justify-content-center text-center" >
+                        <p class="custom-font" style="text-shadow:2px 2px 4px rgba(0, 0, 0, 0.3);">Hot Deals For You</p>
+                   </div>
+
+                    <div class="col-4 d-flex flex-column align-items-center justify-content-center text-center" >
+                       <img src="img/2.png" width="340px">
+                    </div>
+              </div>
+        </div>
+       <div class="container py-5 custom-container">
+            <div class="row align-items-center g-4">
+                <div class="col-4 d-flex flex-column align-items-end justify-content-end">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/Quality.png" class="card-img-top">
+                        <div class="card-body">
+                        <h5 class="card-title">Quality</h5>
+                        <p class="card-text">guarantee all goods are of the best quality.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-4 d-flex flex-column align-items-center justify-content-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/save-money.png" class="card-img-top">
+                        <div class="card-body">
+                        <h5 class="card-title">Save Money</h5>
+                        <p class="card-text">guarantee all goods are of the best quality</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-4 d-flex flex-column align-items-baseline justify-content-around">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/conversation.png" class="card-img-top">
+                        <div class="card-body">
+                        <h5 class="card-title">Contact With Us</h5>
+                        <p class="card-text">guarantee all goods are of the best quality</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- This Week Populer Cloths -->
+           <div class="container mt-5">
+                <p class="custom-font d-flex flex-column align-items-center justify-content-center text-center" style="text-shadow:2px 2px 4px rgba(0, 0, 0, 0.3);">This Week Populer Cloths</p>
+                 <div class="row section-container align-items-center">
+                    <div class="row section-container align-items-center">
+                        <!-- Text Box -->
+                        <div class="col-md-6">
+                             <div class="feature-box">
+                                    <p>
+                                    Being able to sit back and relax in your favourite armchair is truly one of the greatest feelings in life.
+                                    That’s why we’ve created a stunning range of lovingly handmade luxury armchairs, using only materials of the
+                                    finest quality, to ensure you have the best seat in the house.
+                                    </p>
+                                    <button class="btn btn-primary">Add to Cart</button>
+                             </div>
+                        </div>
+
+                        <div class="col-md-6"></div>
+                            <img src="img/baju.png" alt="Baju" class="baju-float">
+                        <div class="deco-top-right d-none d-md-block"></div>
+                    </div>
+
+                    <div class="row  section-container align-items-center flex-md-row-reverse">
+                        <div class="col-md-6">
+                            <div class="feature-box">
+                                <p>
+                                As in the best seat in the house. Combining comfort and creativity, our stunning range of armchair features
+                                designs to suit any style. Perhaps you’re looking for a mid-century masterpiece.
+                                </p>
+                                <button class="btn btn-warning">Add to Cart</button>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6"></div>
+                            <img src="img/baju2.png" alt="Baju" class="baju-float-left" style="top: -40px;">
+                        <div class="deco-bottom-left d-none d-md-block"></div>
+                    </div>
+                </div>
+           </div>
+
+        <!-- Wooden clots -->
+        <div class="container-fluid carousel-container text-center">
+                <p class="custom-font d-flex flex-column align-items-center justify-content-center text-center"
+                style="text-shadow:2px 2px 4px rgba(0, 0, 0, 0.3);">Wooden Clots seets</p>
+                <div class="carousel-wrapper">
+                    <button class="arrow-btn arrow-left" onclick="prevSlide()">&#x2039;</button>
+                    <div class="carousel-inner" id="customCarousel">
+                        <img src="img/baju.png" class="carousel-img-small" alt="Left">
+                        <img src="img/baju2.png" class="carousel-img-active" alt="Center">
+                        <img src="img/baju2.png" class="carousel-img-small" alt="Right">
+                    </div>
+                    <button class="arrow-btn arrow-right" onclick="nextSlide()">&#x203A;</button>
+                </div>
+        </div>
+
+    <script>
+        const images = [
+            ["img/baju.png", "img/baju2.png", "img/baju2.png"],
+            ["img/baju2.png", "img/baju2.png", "img/baju.png"],
+            ["img/baju2.png", "img/baju.png", "img/baju2.png"]
+        ];
+        let index = 0;
+
+        function renderCarousel() {
+            const container = document.getElementById('customCarousel');
+            container.innerHTML = `
+            <img src="${images[index][0]}" class="carousel-img-small" alt="Left">
+            <img src="${images[index][1]}" class="carousel-img-active" alt="Center">
+            <img src="${images[index][2]}" class="carousel-img-small" alt="Right">
+            `;
+        }
+        function nextSlide() {
+            index = (index + 1) % images.length;
+            renderCarousel();
+        }
+
+        function prevSlide() {
+            index = (index - 1 + images.length) % images.length;
+            renderCarousel();
+        }
+    </script>
+
+    <!-- This Week Populer Cloths -->
+    <div class="container mt-5">
+        <p class="custom-font d-flex flex-column align-items-center justify-content-center text-center" style="text-shadow:2px 2px 4px rgba(0, 0, 0, 0.3);">This Week Populer Cloths</p>
+            <div class="row justify-content-center">
+                <div class="col-1 custom-mg" >
+                    <button type="button" class="btn btn-outline-custom">Primary</button>
+                </div>
+                <div class="col-1 custom-mg">
+                    <button type="button" class="btn btn-outline-custom">Primary</button>
+                </div>
+                <div class="col-1 custom-mg">
+                    <button type="button" class="btn btn-outline-custom">Primary</button>
+                </div>
+                <div class="col-1 custom-mg">
+                    <button type="button" class="btn btn-outline-custom">Primary</button>
+                </div>
+                <div class="col-1 custom-mg">
+                    <button type="button" class="btn btn-outline-custom">Primary</button>
+                </div>
+                <div class="col-1 custom-mg">
+                    <button type="button" class="btn btn-outline-custom">Primary</button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    <a href="deskripsi.html" class="text-decoration-none text-dark">
+                    <div class="card" style="width: 18rem; cursor: pointer;">
+                        <img src="img/baju2.png" class="card-img-top" alt="Produk Baju">
+                        <div class="card-body">
+                        <h5 class="card-title">Kemeja Corduroy</h5>
+                        <p class="card-text">Kemeja stylish berbahan corduroy cocok untuk gaya kasual maupun formal.</p>
+                        <p class="card-text fw-bold">Rp249.000</p>
+                        </div>
+                    </div>
+                    </a>
+
+                </div>
+                <div class="col-3">
+
+                </div>
+                <div class="col-3">
+
+                </div>
+                <div class="col-3">
+
+                </div>
+            </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
